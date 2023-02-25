d4310ebcc0ec32245472997c60f6d16fa6bc9ac4
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-25 20:00:12 +0530

    task 1 sign up

---
+++ App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header/Header'
+import SignUp from './pages/SignUp'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <SignUp />
+    </div>
+  )
+}
+
+export default App
